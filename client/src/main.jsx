34286cc0fb0dcb2d41d8aa50279e1907ce72d9eb
Nabin Poudel <nabinpoudel788@gmail.com>
2025-02-05 08:09:37 +0545

    Initialize personal blogger website

---
+++ client/src/main.jsx
@@ -0,0 +1,11 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { StrictMode } from "react";
+import {createRoot} from "react-dom";
+import { App } from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
