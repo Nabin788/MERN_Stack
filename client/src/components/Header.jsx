67c38ecf7a1edc12e013bed7d8b216ccf5a34d79
Nabin Poudel <nabinpoudel788@gmail.com>
2025-02-09 00:39:47 +0545

    create registration form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,18 +7,19 @@ export const HeaderSection = () => {
     const path = useLocation().pathname;
     return (
         <header>
-            <Navbar className="border-b-2">
-                <Link to="/" className="flex gap-1 self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
-                <img src="/nabin.webp" alt="Nabin Poudel Photo" className="w-9 h-9 rounded-lg"/>
-                <span className="px-2 py-2 bg-black text-white rounded">Nabin Poudel</span>
+            <Navbar className="!bg-transparent
+             border-b-2">
+                <Link to="/" className="flex gap-1 self-center items-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+                    <img src="/nabin.webp" alt="Nabin Poudel Photo" className="w-9 h-9 rounded-lg" />
+                    <span className="px-2 py-2 bg-black text-white rounded">Nabin Poudel</span>
                 </Link>
 
                 <form className="hidden lg:inline">
-                    <TextInput 
-                    type="text" 
-                    placeholder="Search..." 
-                    rightIcon={AiOutlineSearch}
-                    className="hiddden lg:inline" />
+                    <TextInput
+                        type="text"
+                        placeholder="Search..."
+                        rightIcon={AiOutlineSearch}
+                        className="hiddden lg:inline" />
                 </form>
 
                 <Button className="w-12 h-10 lg:hidden" color="gray" pill>
@@ -30,31 +31,33 @@ export const HeaderSection = () => {
                         <FaMoon />
                     </Button>
                     <Link to="login">
-                    <Button className="text-black border-black" outline>Login</Button></Link>
+                        <Button className="text-blue-500 border-black" outline>Login</Button></Link>
                     <Navbar.Toggle />
                 </div>
 
-                <Navbar.Collapse> 
-                        <Navbar.Link active={path === "/"} as={"div"}>
-                            <Link to="/">Home</Link>
-                        </Navbar.Link>
+                <Navbar.Collapse>
+                    <Navbar.Link active={path === "/"} as={"div"}>
+                        <Link to="/" className={`text-black ${path === "/" ? "text-blue-500" : "hover:text-blue-500"}`}>Home</Link>
+                    </Navbar.Link>
 
-                        <Navbar.Link active={path === "/about"}  as={"div"}>
-                            <Link to="/about">About</Link>
-                        </Navbar.Link>
+                    <Navbar.Link active={path === "/about"} as={"div"}>
+                        <Link to="/about" className={`text-black ${path === "/about" ? "text-blue-500" : "hover:text-blue-500"}`}>About</Link>
+                    </Navbar.Link>
 
-                        <Navbar.Link active={path === "/projects"}  as={"div"}>
-                            <Link to="/projects">Projects</Link>
-                        </Navbar.Link>
+                    <Navbar.Link active={path === "/projects"} as={"div"}>
+                        <Link to="/projects" className={`text-black ${path === "/projects" ? "text-blue-500" : "hover:text-blue-500"}`}>Projects</Link>
+                    </Navbar.Link>
 
-                        <Navbar.Link active={path === "/blog"}  as={"div"}>
-                            <Link to="/blog">Blog</Link>
-                        </Navbar.Link>
+                    <Navbar.Link active={path === "/blog"} as={"div"}>
+                        <Link to="/blog" className={`text-black ${path === "/blog" ? "text-blue-500" : "hover:text-blue-500"}`}>Blog</Link>
+                    </Navbar.Link>
 
-                        <Navbar.Link active={path === "/contact"}  as={"div"}>
-                            <Link to="/contact">Contact</Link>
-                        </Navbar.Link>
-                    </Navbar.Collapse>
+                    <Navbar.Link active={path === "/contact"} as={"div"}>
+                        <Link to="/contact" className={`text-black ${path === "/contact" ? "text-blue-500" : "hover:text-blue-500"}`}>Contact</Link>
+                    </Navbar.Link>
+                </Navbar.Collapse>
+
+
             </Navbar>
         </header>
     );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,40 @@
+import {Link} from "react-router-dom";
+import {Button, Label, TextInput} from "flowbite-react";
+
 export const RegisterPage = () => {
     return (
-        <h1>Welcome to my RegisterPage</h1>
+        <div className="min-h-screen mt-20">
+            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5 ">
+                <div className="flex-1 self-center ">
+                    <Link to="/" className="font-bold dark:text-white text-4xl">
+                <span className="px-2 py-1 bg-gradient-to-r from-indigo-500
+                 via-purple-500 to-pink-500 rounded rounded-lg text-white">Nabin Portfolio</span>
+                    </Link>
+                </div>
+                <div className="m-4 flex-1">
+                    <form className="flex flex-col gap-5">
+                        <div>
+                            <Label value="Your username"/>
+                            <TextInput type="text" placeholder="Username" id="username"/>
+                        </div>
+                        <div>
+                            <Label value="Your email"/>
+                            <TextInput type="email" placeholder="example@gmail.com" id="email"/>
+                        </div>
+                        <div>
+                            <Label value="Your password"/>
+                            <TextInput type="password" placeholder="Password" id="password"/>
+                        </div>
+                        <Button gradientDuoTone="purpleToPink" type="submit">Sing Up</Button>
+                    </form>
+                    <div className="flex gap-2 text-sm mt-5">
+                        <span>have an account?</span>
+                        <Link to="/register" className="text-blue-500">
+                            Sign In
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
